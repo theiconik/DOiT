53f984d9b6b5c6f7e2ba5f8d763fca8df8e2d6c2
theiconik <kumarujjawal51@gmail.com>
2020-12-29 18:30:27 +0530

    Added code to append todo list to todo div

--- app.js
+++ app.js
@@ -11,4 +11,24 @@ todoButton.addEventListener('click', addTodo)
 function addTodo(event){
    // Prevent form from Submitting
    event.preventDefault();
+   //Todo DIV
+   const todoDiv= document.createElement('div');
+   todoDiv.classList.add("todo")
+   //Create LI
+   const newTodo = document.createElement('li');
+   newTodo.innerText= 'Hey';
+   newTodo.classList.add('todo-item');
+   todoDiv.appendChild(newTodo);
+   //Check Mark Button
+   const completedButton = document.createElement('button');
+   completedButton.innerHTML = '<i class="fas fa-check"></i>';
+   completedButton.classList.add("complete-btn");
+   todoDiv.appendChild(completedButton);
+   //Trash Button
+   const trashButton = document.createElement('button');
+   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+   trashButton.classList.add("complete-btn");
+   todoDiv.appendChild(trashButton);
+   //Append to list
+   todoList.appendChild(todoDiv)
 }
